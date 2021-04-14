727ad73b40b6a1e300487726e0bd469990ec6873
Marius Hambro Bovbjerg <marius@hambro.eu>
2021-04-14 11:53:03 +0200

    Added some bogus

---
+++ Library/DataGenerator/DataGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Models.FamilyMembers;
+using Library.Models.Births;
+
+namespace Library.DataGenerator
+{
+    class DataGenerator
+    {   
+        public enum FamilyMemberType
+        {
+            FATHER,
+            MOTHER,
+            CHILD,
+            RELATIVE
+        }
+
+        public FamilyMember CreateFakeFamilyMember()
+        {
+        
+            var faker = new Faker("en");
+            var o = new FamilyMember()
+            {
+                FirstName = faker.Name.FirstName(),
+                LastName = faker.Name.LastName(),
+                MemberType = (Models.FamilyMembers.FamilyMemberType)faker.PickRandom<FamilyMemberType>()
+            };
+            return o;
+        }
+        public Birth CreateFakeBirth()
+        {
+
+            var faker = new Faker("en");
+            var o = new FamilyMember()
+            {
+                FirstName = faker.Name.FirstName(),
+                LastName = faker.Name.LastName(),
+                MemberType = (Models.FamilyMembers.FamilyMemberType)faker.PickRandom<FamilyMemberType>()
+            };
+            return o;
+        }
+    }
+}
